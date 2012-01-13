54fe0b9effc409763a52fcdb0d4d8164e709406c
David Sterry <david@sterryit.com>
2012-01-13 15:38:04 -0800

    fixed typo

--- includes/languages/english/modules/payment/bitcoin.php
+++ includes/languages/english/modules/payment/bitcoin.php
@@ -3,6 +3,6 @@
 //Bitcoin Payment Module - Lanugage File
   define('MODULE_PAYMENT_BITCOIN_TEXT_TITLE', 'Bitcoin Payment');
   define('MODULE_PAYMENT_BITCOIN_TEXT_DESCRIPTION', 'Bitcoin Payment');
-  define('MODULE_PAYMENT_BITCOIN_TEXT_EMAIL_FOOTER', 'Payed with Bitcoins - Thank you!');
+  define('MODULE_PAYMENT_BITCOIN_TEXT_EMAIL_FOOTER', 'Paid with Bitcoins - Thank you!');
 
 ?>
